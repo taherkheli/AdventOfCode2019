a6c35d9b9d32e05e94f7f0343f2741dbe5254960
Bilal Taherkheli <bilal76@gmail.com>
2019-12-08 22:13:12 +0100

    removed stupidities. it all works now

--- Day01/Part1/Part1.cs
+++ Day01/Part1/Part1.cs
@@ -14,14 +14,14 @@ namespace Part1
     private static void Calculate(string inputFile)
     {
       string line;
-      ulong fuelNeeded = 0;
-      ulong sum = 0;
+      long fuelNeeded;
+      long sum = 0;
 
       StreamReader file = new StreamReader(inputFile);
 
       while ((line = file.ReadLine()) != null)
       {
-        fuelNeeded = CalculateFuel(ulong.Parse(line));
+        fuelNeeded = CalculateFuel(long.Parse(line));
         sum += fuelNeeded;
       }
 
@@ -29,17 +29,12 @@ namespace Part1
       file.Close();
     }
 
-    private static ulong CalculateFuel(ulong val)
+    private static long CalculateFuel(long val)
     {
-      ulong result = 0;
+      long result = 0;
 
-      result = val/3;
-
-      if (result > 2)   //avoid -ve
-        result -= 2;
-
-      if (result == 1)  //special case; avoid returning 1
-        result = 0;
+      if (val > 6)
+        result = (val/3)-2;
 
       return result;
     }
--- Day01/Part2/Part2.cs
+++ Day01/Part2/Part2.cs
@@ -14,14 +14,14 @@ namespace Part2
     private static void Calculate(string inputFile)
     {
       string line;
-      ulong fuelNeeded = 0;
-      ulong sum = 0;
+      long fuelNeeded = 0;
+      long sum = 0;
 
       StreamReader file = new StreamReader(inputFile);
 
       while ((line = file.ReadLine()) != null)
       {
-        fuelNeeded = CalculateFuelCumulatively(ulong.Parse(line));
+        fuelNeeded = CalculateFuelCumulatively(long.Parse(line));
         sum += fuelNeeded;
       }
 
@@ -29,36 +29,26 @@ namespace Part2
       file.Close();
     }
 
-    private static ulong CalculateFuelCumulatively(ulong val)
+    private static long CalculateFuelCumulatively(long val)
     {
-      ulong sum = 0;
-      ulong next = val;
+      long sum = 0;
+      long next = val;
       
       while (true)
       {
         next = CalculateFuel(next);        
         sum += next;
 
-        if (next < 1)
+        if (next < 6)  //no point botherin
           break;
       }
 
       return sum;
     }
 
-    private static ulong CalculateFuel(ulong val)
+    private static long CalculateFuel(long val)
     {
-      ulong result = 0;
-
-      result = val / 3;
-            
-      if (result > 2)   //avoid -ve
-        result -= 2;
-      
-      if (result == 1)  //special case; avoid returning 1
-        result = 0;
-
-      return result;
+      return ((val/3)-2);
     }
   }
 }
