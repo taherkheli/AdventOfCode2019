26fa88e18c7a300e476ba3de4bcf9a18cbe9d31b
Bilal Taherkheli <bilal76@gmail.com>
2019-12-08 02:13:31 +0100

    no performance but correct answer :(

---
+++ Day03/Day03/CrossingWires/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CrossingWires
+{
+  class Program
+  {
+    static void Main()
+    {
+      Wire wire1 = new Wire();
+      Wire wire2 = new Wire();
+      string path = "input.txt";
+
+      StreamReader file = new StreamReader(path);
+      string[] strings = file.ReadToEnd().Split((char)ConsoleKey.Enter);
+
+      wire1.Move(LoadInput(strings[0]));
+      wire2.Move(LoadInput(strings[1].Substring(1)));  //get rid of '\n' in second string
+
+      var crosses = wire1.FindAllCrossingsWith(wire2);
+
+      Console.WriteLine(CalculateManhattanDistance(crosses));
+    }
+
+    private static List<Movement> LoadInput(string path)
+    {
+      List<Movement> result = new List<Movement>();
+
+      string[] strings = path.Split(',');
+
+      foreach (var str in strings)
+        result.Add(ExtractMovement(str));
+
+      return result;
+    }
+
+    private static Movement ExtractMovement(string str)
+    {
+      Movement result = new Movement { };
+      var firstChar = str[0];      
+      result.StepSize = int.Parse(str.Trim(firstChar));
+
+      switch (firstChar)
+      {
+        case 'U':
+          result.Direction = Direction.Up;
+          break;
+        case 'D':
+          result.Direction = Direction.Down;
+          break;
+        case 'L':
+          result.Direction = Direction.Left;
+          break;
+        case 'R':
+          result.Direction = Direction.Right;
+          break;
+        default:
+          throw new InvalidOperationException("Wrong input!");
+      }
+
+      return result;      
+    }
+
+    private static int CalculateManhattanDistance(List<Point> points)
+    {
+      int[] distances = new int[points.Count];
+
+      for (int i = 0; i < points.Count; i++)
+        distances[i] = Math.Abs(points[i].X) + Math.Abs(points[i].Y);
+
+      Array.Sort(distances);
+
+      return distances[0];
+    }
+  }
+}
---
+++ Day03/Day03/CrossingWires/Wire.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossingWires
+{
+  public class Wire
+  {
+    private Point _head;
+
+    private readonly List<Point> _points;
+
+    public Wire()
+    {
+      _head = new Point { X = 0, Y = 0 };
+      _points = new List<Point>
+      {
+        _head
+      };
+    }
+
+    public Point Head { get => _head; }
+    public List<Point> Points { get => _points; }
+
+    public void Move(List<Movement> movements)
+    {
+      foreach (var movement in movements)
+        TrackWire(movement);
+    }
+
+    public bool HasCrossed(Point p)
+    {
+      return _points.Contains(p) ? true : false;
+    }
+
+    public List<Point> FindAllCrossingsWith(Wire otherWire)
+    {
+      var result = new List<Point>();
+
+      foreach (var p in _points)
+      {
+        if (otherWire.HasCrossed(p))
+          result.Add(p);
+
+        //TODO: added this just to get to to some result. need soem performance optimization it seems ;) but at least I got the answer right        
+        if (result.Count > 10)
+          break;
+      }
+
+      result.Remove(new Point() { X = 0, Y = 0 });   //ignore origin
+      return result;
+    }
+
+    private void TrackWire(Movement movement)
+    {
+      switch (movement.Direction)
+      {
+        case Direction.Up:
+          for (int i = 1; i <= movement.StepSize; i++)
+          {
+            _head.Y++;
+            _points.Add(_head);
+          }
+          break;
+
+        case Direction.Down:
+          for (int i = 1; i <= movement.StepSize; i++)
+          {
+            _head.Y--;
+            _points.Add(_head);
+          }
+          break;
+
+        case Direction.Left:
+          for (int i = 1; i <= movement.StepSize; i++)
+          {
+            _head.X--;
+            _points.Add(_head);
+          }
+          break;
+
+        case Direction.Right:
+          for (int i = 1; i <= movement.StepSize; i++)
+          {
+            _head.X++;
+            _points.Add(_head);
+          }
+          break;
+
+        default:
+          break;
+      }
+
+    }
+  }
+}
