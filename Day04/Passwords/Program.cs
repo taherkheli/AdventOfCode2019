488da9ba478ae4bd97b200ac8792111e2c68ab6d
Bilal Taherkheli <bilal76@gmail.com>
2019-12-10 02:47:29 +0100

    quite embarassing code and I am sure theres an elegant way of doing this but it works and its 02:47 am :(

--- Day04/Passwords/Program.cs
+++ Day04/Passwords/Program.cs
@@ -9,7 +9,6 @@ namespace Passwords
     {
       int lb = 359282;
       int ub = 820401;
-
       Console.WriteLine("Possible passwords : {0}", PossiblePasswords(lb, ub));
     }
 
@@ -29,7 +28,7 @@ namespace Passwords
     private static bool ConditionsAreMet(int number)
     {
       bool digitsNeverDecrease = false;
-      bool doubleExists = false;
+      bool doubleExistsAndMeetsCriteria = false;
 
       char[] chars = number.ToString().ToCharArray();
       int[] digits = new int[chars.Length];
@@ -47,16 +46,74 @@ namespace Passwords
         }
       }
 
-      for (int i = 0; i < digits.Length - 1; i++)
+      if (digitsNeverDecrease)
       {
-        if (digits[i] == digits[i + 1])
+        for (int i = 0; i < digits.Length - 1; i++)
         {
-          doubleExists = true;
-          break;
+          if (digits[i] == digits[i + 1])
+          {
+            if (CheckDoubles(digits[i], digits))
+            {
+              doubleExistsAndMeetsCriteria = true;
+              break;
+            }
+            else
+              continue;
+          }
         }
       }
 
-      return (doubleExists && digitsNeverDecrease);
+      return (doubleExistsAndMeetsCriteria && digitsNeverDecrease);
+    }
+
+    //will only be called when a double is found
+    private static bool CheckDoubles(int digit, int[] digits)
+    {
+      int adjacancies = 2;   //or method won't be called
+      int startIndex = 0;
+      bool result = false;
+
+      for (int i = 0; i < digits.Length; i++)
+      {
+        if ((digits[i] == digit) && (i<5))   //no point beyond index 4 as it means double is at index 5 and 6
+        {
+          startIndex = i + 2;
+          break;
+        }
+      }
+     
+      for (int i = startIndex; i < digits.Length; i++)
+      {
+        if (digits[i] == digit)
+          adjacancies++;
+      }
+
+      switch (adjacancies)
+      {
+        case 2:
+          result = true;
+          break;
+
+        case 4:
+          if ((digits[4] > digit) && (digits[4] == digits[5]))
+          {
+            result = true;
+            break;
+          }
+          else
+          {
+            result = false;
+            break;
+          }
+
+        case 3:
+        case 5:
+        default:
+          result = false;
+          break;
+      }
+
+      return result;
     }
   }
 }
