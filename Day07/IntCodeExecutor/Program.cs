df0f0af32a6f2f82bbd5e0e31d0ef0f67ffa0578
Bilal Taherkheli <bilal76@gmail.com>
2020-02-02 19:34:43 +0100

    Reafctored to ease unit testing. Added unit tests because JLo said so an dfor once he was right!

--- Day07/IntCodeExecutor/Program.cs
+++ Day07/IntCodeExecutor/Program.cs
@@ -7,39 +7,15 @@ namespace IntCodeExecutorNs
   {
     static void Main()
     {
-
       string path = "input.txt";
       int[] program = LoadInput(path);
       int[] sequence = new int[5] { 1, 0, 4, 3, 2 };
       int highest = -1;
-      var permutations = PermutationCalculator.GetCombinations(sequence);
-
-      foreach (var permutation in permutations)
-      {
-        var temp = Calculate(program, permutation.ToArray());
-        if (temp > highest)
-          highest = temp;
-      }
+      highest = Helpers.GetHighest(program, sequence);
 
       Console.WriteLine("\n strongest signal value :  {0}", highest);
     }
-
-    private static int Calculate(int[] program, int[] pattern)
-    {
-      IntCodeExecutor amp1 = new IntCodeExecutor(program, new int[] { pattern[0], 0 });
-      amp1.Execute();
-      IntCodeExecutor amp2 = new IntCodeExecutor(program, new int[] { pattern[1], (int)amp1.Output });
-      amp2.Execute();
-      IntCodeExecutor amp3 = new IntCodeExecutor(program, new int[] { pattern[2], (int)amp2.Output });
-      amp3.Execute();
-      IntCodeExecutor amp4 = new IntCodeExecutor(program, new int[] { pattern[3], (int)amp3.Output });
-      amp4.Execute();
-      IntCodeExecutor amp5 = new IntCodeExecutor(program, new int[] { pattern[4], (int)amp4.Output });
-      amp5.Execute();
-
-      return (int)amp5.Output;
-    }
-
+    
     private static int[] LoadInput(string path)
     {
       StreamReader file = new StreamReader(path);
--- Day07/IntCodeExecutorTest/InstDecoderTest.cs
+++ Day07/IntCodeExecutorTest/InstDecoderTest.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using IntCodeExecutorNs;
 
-namespace IntCodeExecutorTest
+namespace InstDecoderTest
 {
   [TestClass]
   public class UnitTest
