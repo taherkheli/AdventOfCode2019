1993d46892f6a10d89cc25c0a656d000848fe678
Bilal Taherkheli <bilal76@gmail.com>
2020-02-15 19:14:06 +0100

    maybe 4 GetPoints methods can be merged into 1 as well but frankly I don't care. Cleaned up as much as was obvious

--- Day10/AsteroidDetection/Program.cs
+++ Day10/AsteroidDetection/Program.cs
@@ -27,107 +27,46 @@ namespace AsteroidDetection
           actualAsteroid = grid.Asteroids[i];
         }
       }
-
+      
       var X = actualAsteroid.Position.X;
       var Y = actualAsteroid.Position.Y;
       Console.WriteLine("\nAsteroid on position ({0},{1}) wins as it can detect:  {2}\n", X, Y, highest);
-
-
+      
       /************* Part II ****************/
 
       Point center = new Point(actualAsteroid.Position.X, actualAsteroid.Position.Y);
       int vaporizedCount = 0;
-      int rotationCount = 0;
 
       while (grid.Asteroids.Count > 1) //all except the monitoring station
       {
-        rotationCount++;
-        Console.WriteLine("\nRotation #: {0}", rotationCount);
+        vaporizedCount = Vaporize(grid, center, vaporizedCount, grid.GetPointsListQ1(center));    
+        vaporizedCount = Vaporize(grid, center, vaporizedCount, grid.GetPointsListQ2(center));
+        vaporizedCount = Vaporize(grid, center, vaporizedCount, grid.GetPointsListQ3(center));
+        vaporizedCount = Vaporize(grid, center, vaporizedCount, grid.GetPointsListQ4(center));
+      }
+    }
 
-        //process Quad1
-        Console.WriteLine("\nProcessing quadrant 1");
-        var pointsOfInterest = grid.GetPointsListQ1(center);    //points of interest represent one point from each line the laser will shoot at during rotation
-        foreach (var p in pointsOfInterest)
+    private static int Vaporize(Grid grid, Point center, int vaporizedCount, List<Point> pointsOfInterest)
+    {
+      foreach (var p in pointsOfInterest)
+      {
+        var lineOfSight = grid.GetLineOfSight(center, p);
+
+        for (int i = 0; i < lineOfSight.Count; i++)
         {
-          var lineOfSight = grid.GetLineOfSight(center, p);
+          var asteroid = grid.Asteroids.Find(a => (a.Position.X == lineOfSight[i].X) && (a.Position.Y == lineOfSight[i].Y));
 
-          for (int i = 0; i < lineOfSight.Count; i++)
+          if (asteroid != null)
           {
-            var asteroid = grid.Asteroids.Find(a => (a.Position.X == lineOfSight[i].X) && (a.Position.Y == lineOfSight[i].Y));
-            
-            if (asteroid != null)
-            {
-              vaporizedCount++;
-              grid.Asteroids.Remove(asteroid);
-              Console.WriteLine("Asteroid #{0} to be vaporized is at ({1},{2})", vaporizedCount, asteroid.Position.X, asteroid.Position.Y);
-              break;
-            }
-          }
-        }
-
-        //process Quad2
-        Console.WriteLine("\nProcessing quadrant 2");
-        pointsOfInterest = grid.GetPointsListQ2(center);
-        foreach (var p in pointsOfInterest)
-        {
-          var lineOfSight = grid.GetLineOfSight(center, p);
-
-          for (int i = 0; i < lineOfSight.Count; i++)
-          {
-            var asteroid = grid.Asteroids.Find(a => (a.Position.X == lineOfSight[i].X) && (a.Position.Y == lineOfSight[i].Y));
-
-            if (asteroid != null)
-            {
-              vaporizedCount++;
-              grid.Asteroids.Remove(asteroid);
-              Console.WriteLine("Asteroid #{0} to be vaporized is at ({1},{2})", vaporizedCount, asteroid.Position.X, asteroid.Position.Y);
-              break;
-            }
-          }
-        }
-
-        //process Quad3
-        Console.WriteLine("\nProcessing quadrant 3");
-        pointsOfInterest = grid.GetPointsListQ3(center);        
-        foreach (var p in pointsOfInterest)
-        {
-          var lineOfSight = grid.GetLineOfSight(center, p);
-
-          for (int i = 0; i < lineOfSight.Count; i++)
-          {
-            var asteroid = grid.Asteroids.Find(a => (a.Position.X == lineOfSight[i].X) && (a.Position.Y == lineOfSight[i].Y));
-
-            if (asteroid != null)
-            {
-              vaporizedCount++;
-              grid.Asteroids.Remove(asteroid);
-              Console.WriteLine("Asteroid #{0} to be vaporized is at ({1},{2})", vaporizedCount, asteroid.Position.X, asteroid.Position.Y);
-              break;
-            }
-          }
-        }
-
-        //process Quad4
-        Console.WriteLine("\nProcessing quadrant 4");
-        pointsOfInterest = grid.GetPointsListQ4(center);    
-        foreach (var p in pointsOfInterest)
-        {
-          var lineOfSight = grid.GetLineOfSight(center, p);
-
-          for (int i = 0; i < lineOfSight.Count; i++)
-          {
-            var asteroid = grid.Asteroids.Find(a => (a.Position.X == lineOfSight[i].X) && (a.Position.Y == lineOfSight[i].Y));
-
-            if (asteroid != null)
-            {
-              vaporizedCount++;
-              grid.Asteroids.Remove(asteroid);
-              Console.WriteLine("Asteroid #{0} to be vaporized is at ({1},{2})", vaporizedCount, asteroid.Position.X, asteroid.Position.Y);
-              break;
-            }
+            vaporizedCount++;
+            grid.Asteroids.Remove(asteroid);
+            Console.WriteLine("Asteroid #{0} to be vaporized is at ({1},{2})", vaporizedCount, asteroid.Position.X, asteroid.Position.Y);
+            break;
           }
         }
       }
+
+      return vaporizedCount;
     }
   }
 }
