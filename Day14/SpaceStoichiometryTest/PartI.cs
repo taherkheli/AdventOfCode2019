a76078057458ae5f8256ff566492528523fa046b
Bilal Taherkheli <bilal76@gmail.com>
2020-03-25 21:40:54 +0100

    second test case passes as well. I expect no more though ;)

--- Day14/SpaceStoichiometryTest/PartI.cs
+++ Day14/SpaceStoichiometryTest/PartI.cs
@@ -29,5 +29,30 @@ namespace SpaceStoichiometryTest
 
       Assert.AreEqual(expected, actual, "OK!");
     }
+
+    [TestMethod]
+    public void TC02()
+    {
+      int expected = 165;
+      string[] input = new string[]
+      {
+        "9 ORE => 2 A",
+        "8 ORE => 3 B",
+        "7 ORE => 5 C",
+        "3 A, 4 B => 1 AB",
+        "5 B, 7 C => 1 BC",
+        "4 C, 1 A => 1 CA",
+        "2 AB, 3 BC, 4 CA => 1 FUEL"
+      };
+
+      var reactions = Parser.GetReactions(input);
+      var fuelReaction = reactions.Find(r => r.Output.Name == "FUEL");
+      if (fuelReaction != null)
+        reactions.Remove(fuelReaction);
+
+      int actual = fuelReaction.CalculateNeededOre(reactions);
+
+      Assert.AreEqual(expected, actual, "OK!");
+    }
   }
 }
